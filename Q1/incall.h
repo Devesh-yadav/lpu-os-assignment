671437a56b349cf3c6813b5f107627dd1e220c77
Faisal Alam <faisal.11616526@lpu.in>
2018-03-24 00:56:11 +0530

    Initial commit.

---
+++ Q1/incall.h
@@ -0,0 +1,8 @@
+//
+// Created by Faisal Alam on 24-03-2018.
+//
+
+#ifndef ASSIGNMENT_INCALL_H
+#define ASSIGNMENT_INCALL_H
+
+#endif //ASSIGNMENT_INCALL_H
---
+++ Q32/main.c
@@ -0,0 +1,11 @@
+//
+// Created by Faisal Alam on 24-03-2018.
+//
+
+#include "incall.h"
+
+int main() {
+    // TODO: Implement logic here.
+
+    return 0;
+}
