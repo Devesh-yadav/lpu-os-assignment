d29be4e388685709b59eb0756d105177b089a33b
Faisal Alam <faisal.11616526@gmail.com>
2018-03-25 00:22:37 +0530

    Implemented user input.

--- Q1/main.c
+++ Q1/main.c
@@ -5,7 +5,36 @@
 #include "incall.h"
 
 int main() {
-    // TODO: Implement logic here.
+    pid_t p; // Store the process ID.
+    int n; // An integer variable to store the input.
+    short err_flag = 0; // A flag variable to detect bad input.
+
+    do {
+        if (err_flag == 1)
+            printf("\nBad input! Please enter a number greater than 1.");
+
+        printf("\nEnter any number: ");
+        scanf("%d", &n);
+
+        err_flag = 1; // Set the error flag to 1, to display error message on next iteration.
+
+    } while (n < 1);
+
+    err_flag = 0; // Reset the error flag.
+
+    p = fork(); // Create a child process.
+
+    switch (p) {
+        case -1:
+            // Process creation failed.
+            printf("\n\nFailed to create the child process.");
+            exit(1); // Abort the program.
+        case 0:
+            // TODO: Implement logic here.
+            break;
+        default:
+            wait();
+    }
 
     return 0;
 }
