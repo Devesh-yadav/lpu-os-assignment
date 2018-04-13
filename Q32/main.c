e988ddeaacef7cf2027e30af59a39487374ad03c
Faisal Alam <faisal.11616526@gmail.com>
2018-04-14 00:19:55 +0530

    Zombie process demonstration.

--- Q32/main.c
+++ Q32/main.c
@@ -5,7 +5,37 @@
 #include "incall.h"
 
 int main() {
-    // TODO: Implement logic here.
+    pid_t p[2]; int i;
+
+    /**
+     * Creation of Zombie Process.
+     * 
+     * A zombie process that has completed execution via the exit system call, but still has an entry in the process table.
+     * In other words, a zombie process is a child process whose parent has died before the child finishes its execution.
+     * So, we create two child processes and makes the parent sleep for 30 seconds (and not wait for the child).
+     * Thus the child process becomes a zombie process.
+     */
+    for (i = 0; i < 2; ++i) {
+        p[i] = fork(); // Create two processes.
+
+        if (p[i] == 0)
+        {
+            // Let the child process exit quickly.
+            printf("Child process having PID %d.\n", getpid());
+            exit(EXIT_SUCCESS);
+        }
+        else if (p[i] > 0)
+        {
+            printf("Parent process having PID: %d.\n", getpid());
+        }
+        else
+        {
+            perror("Fork failed!\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    sleep(30); // Let parent sleep for 30 seconds and exit.
 
     return 0;
 }
