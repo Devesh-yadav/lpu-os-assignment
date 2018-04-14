830dfb919a1f280bca527383a2eda34936656d09
Faisal Alam <faisal.11616526@gmail.com>
2018-04-14 12:46:48 +0530

    Optimized code and corrected definition.

--- Lovely Professional University/Semester 4/CSE316/Q32/zombie.c
+++ Lovely Professional University/Semester 4/CSE316/Q32/zombie.c
@@ -6,7 +6,6 @@
  * Creation of Zombie Process.
  *
  * A zombie process is that process which has completed execution via the exit system call, but still has an entry in the process table.
- * In other words, a zombie process is a child process whose parent has died before the child finishes its execution.
  * So, we create two child processes and makes the parent sleep for 30 seconds (and not wait for the child).
  * Thus the child process becomes a zombie process.
  */
@@ -14,18 +13,18 @@
 #include "incall.h"
 
 int main() {
-    pid_t p[2];
+    pid_t p;
     int i;
 
     for (i = 0; i < 2; ++i) {
-        p[i] = fork(); // Create two processes.
+        p = fork();
 
-        if (p[i] == 0)
+        if (p == 0)
         {
             printf("Child process having PID %d.\n", getpid());
-            exit(EXIT_SUCCESS); // Let the child process exit quickly.
+            exit(EXIT_SUCCESS); // Let the child process exit immediately.
         }
-        else if (p[i] > 0)
+        else if (p > 0)
         {
             printf("Parent process having PID: %d.\n", getpid());
         }
@@ -36,7 +35,7 @@ int main() {
         }
     }
 
-    sleep(30); // Let the parent process sleep for 30 seconds and then exit.
-
-    return 0;
+    // Let the parent process sleep for 30 seconds and then exit.
+    sleep(30);
+    exit(EXIT_SUCCESS);
 }
